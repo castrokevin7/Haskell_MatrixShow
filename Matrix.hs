14071af36e91609b2a3b123cbd4e6e6da44ee419
root <root@localhost.localdomain>
2015-10-23 00:53:37 -0600

    changes

--- Matrix.hs
+++ Matrix.hs
@@ -1,23 +1,23 @@
-module Matrix (Matrix, fillWith, fromRule, numRows, numColumns, at, mtranspose, mmap) where
-newtype Matrix a = Mat ((Int,Int), (Int,Int) -> a)
+module Matrix (Matrix(..), fillWith, fromRule, numRows, numColumns, at, mtranspose, mmap) where
+newtype Matrix a = Mat ((Int, Int), (Int, Int) -> a)
 
-fillWith :: (Int,Int) -> a -> (Matrix a)
-fillWith (n,m) k = Mat ((n,m), (\(_,_) -> k))
+fillWith :: (Int, Int) -> a -> (Matrix a)
+fillWith (n, m) k = Mat ((n, m), (\(_, _) -> k))
 
-fromRule :: (Int,Int) -> ((Int,Int) -> a) -> (Matrix a)
-fromRule (n,m) f = Mat ((n,m), f)
+fromRule :: (Int,Int) -> ((Int, Int) -> a) -> (Matrix a)
+fromRule (n, m) f = Mat ((n, m), f)
 
 numRows :: (Matrix a) -> Int
-numRows (Mat ((n,_),_)) = n
+numRows (Mat ((n, _) ,_)) = n
 
 numColumns :: (Matrix a) -> Int
-numColumns (Mat ((_,m),_)) = m
+numColumns (Mat ((_, m), _)) = m
 
 at :: (Matrix a) -> (Int, Int) -> a
-at (Mat ((n,m), f)) (i,j)| (i > 0) && (j > 0) || (i <= n) && (j <= m) = f (i,j)
+at (Mat ((n, m), f)) (i, j) | (i > 0) && (j > 0) && (i <= n) && (j <= m) = f (i, j)
 
 mtranspose :: (Matrix a) -> (Matrix a)
-mtranspose (Mat ((n,m),f)) = (Mat ((m,n),\(j,i) -> f (i,j)))
+mtranspose (Mat ((n, m), f)) = (Mat ((m, n), \(j, i) -> f (i, j)))
 
 mmap :: (a -> b) -> (Matrix a) -> (Matrix b)
-mmap h (Mat ((n,m),f)) = (Mat ((n,m), h.f))
+mmap h (Mat ((n, m), f)) = (Mat ((n, m), h.f))
--- MatrixShow.hs
+++ MatrixShow.hs
@@ -1,3 +1,10 @@
 module MatrixShow where
 import Matrix
-instance (Show a) => Show(Matrix a) where
+
+instance (Show a) => Show (Matrix a) where 
+
+coords :: (Int, Int) -> [(Int, Int)]
+coords (w, h) = [(x, y) | x <- [0 .. w], y <- [0 .. h]]
+
+elems :: (Matrix a) -> [a]
+elems (Mat (size, f)) = map f $ coords size
